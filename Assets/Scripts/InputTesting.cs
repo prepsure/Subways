567174f2c60f103d0be6f1e756660d21c981ddbd
Paige <33643911+presssssure@users.noreply.github.com>
2023-04-02 02:34:20 -0400

    input is fucked up but it works!!!

---
+++ Assets/Scripts/InputTesting.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Assets.Scripts
+{
+    public class InputTesting : MonoBehaviour
+    {
+        void OnMove(InputValue val)
+        {
+            Debug.Log(gameObject.GetHashCode());
+        }
+
+
+        // Use this for initialization
+        void Update()
+        {
+            
+        }
+    }
+}
